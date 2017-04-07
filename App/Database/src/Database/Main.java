94855ac313fa85ed4309866b49330ae74d9cdef6
JoZonneveld <joostz14@gmail.com>
2017-04-07 17:38:31 +0200

    Database connection

---
+++ App/Database/src/Database/Main.java
@@ -0,0 +1,36 @@
+package Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Connection c = null;
+        Statement stmt = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:Official.db");
+            c.setAutoCommit(false);
+            System.out.println("Opened database successfully");
+
+            stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery( "SELECT * FROM paal;" );
+            while ( rs.next() ) {
+
+                String Adres = rs.getString("Adres");
+
+                System.out.println( "Adres = " + Adres );
+            }
+            rs.close();
+            stmt.close();
+            c.close();
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Operation done successfully");
+    }
+}
