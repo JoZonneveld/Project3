0c27be10717e8402f57a3b328e81b2f1106b6bf6
JoZonneveld <joostz14@gmail.com>
2017-04-10 22:40:25 +0200

    webapp update

--- WebApp/test.php
+++ WebApp/test.php
@@ -1,5 +1,9 @@
 <?php
 
+include 'include/class.php';
+
+$db = new DB();
+
 $db->sql = mysqli_query($db->conn, "SELECT id, Adres, Longitude, Latitude FROM paal");
 
 if ($db->sql->num_rows > 0)
@@ -12,20 +16,7 @@ if ($db->sql->num_rows > 0)
         $lat[$i] = $row['Latitude'];
         $i++;
     }
-}
-
-$db->sql = mysqli_query($db->conn, "SELECT id, AreaDesc, Longitude, Latitude FROM park");
-
-if ($db->sql->num_rows > 0)
-{
-    $i = 0;
-    while($row = $db->sql->fetch_assoc())
-    {
-        $adres1[$i] = $row['AreaDesc'];
-        $long1[$i] = $row['Longitude'];
-        $lat1[$i] = $row['Latitude'];
-        $i++;
-    }
+    print $i;
 }
 
 ?>
@@ -40,39 +31,28 @@ if ($db->sql->num_rows > 0)
     <script src=""></script>
 </head>
 <body>
-<div id="map" style="width:100%;height:400px;"></div>
+<div id="map" style="width: 500px; height: 400px;"></div>
 
 <script>
     var locations = [ ];
-    var locations1 = [ ];
 </script>
 
 <?php
 
-for ($i = 0; $i < count($long); $i++)
-{
-    ?>
-    <script>
-        locations.push(['<?= $adres[$i] ?>', "<?= $lat[$i] ?>", "<?= $long[$i] ?>", <?= $i ?>])
-    </script>
-    <?php
-}
-
-for ($i = 0; $i < count($long1); $i++)
-{
-    ?>
-    <script>
-        locations1.push(['<?= $adres1[$i] ?>', "<?= $lat1[$i] ?>", "<?= $long1[$i] ?>", <?= $i ?>])
-    </script>
-    <?php
-}
+    for ($i = 0; $i < count($long); $i++)
+    {
+        ?>
+            <script>
+                locations.push(['<?= $adres[$i] ?>', "<?= $lat[$i] ?>", "<?= $long[$i] ?>", <?= $i ?>])
+            </script>
+        <?php
+    }
 
 ?>
 
 <script type="text/javascript">
 
-    var paal = 'img/paal1.png';
-    var park = 'img/park.png';
+    //locations.push(['<?= $adres[1] ?>', "<?= $lat[1] ?>", "<?= $long[1] ?>", <?= 1 ?>])
 
 
     var map = new google.maps.Map(document.getElementById('map'), {
@@ -88,28 +68,12 @@ for ($i = 0; $i < count($long1); $i++)
     for (i = 0; i < locations.length; i++) {
         marker = new google.maps.Marker({
             position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-            map: map,
-            icon: paal
+            map: map
         });
 
         google.maps.event.addListener(marker, 'click', (function(marker, i) {
             return function() {
                 infowindow.setContent(locations[i][0]);
-                infowindow.open(map, marker);
-            }
-        })(marker, i));
-    }
-
-    for (i = 0; i < locations1.length; i++) {
-        marker = new google.maps.Marker({
-            position: new google.maps.LatLng(locations1[i][1], locations1[i][2]),
-            map: map,
-            icon: park
-        });
-
-        google.maps.event.addListener(marker, 'click', (function(marker, i) {
-            return function() {
-                infowindow.setContent(locations1[i][0]);
                 infowindow.open(map, marker);
             }
         })(marker, i));
