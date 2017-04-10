117d5a0b7ee6421b380f9e468d46acaad104647c
JoZonneveld <joostz14@gmail.com>
2017-04-10 16:19:23 +0200

    databaseupdate2, delete duplicates2

--- WebApp/delete.php
+++ WebApp/delete.php
@@ -6,7 +6,6 @@ $db->sql = mysqli_query($db->conn, "SELECT * FROM paal ORDER BY Adres");
 
 if ($db->sql->num_rows > 0) {
     $a = 0;
-
     while ($row = $db->sql->fetch_assoc())
     {
         $id = $row['id'];
@@ -14,7 +13,7 @@ if ($db->sql->num_rows > 0) {
             {
                 if($adres[$i] == $row['Adres'])
                 {
-                    print"<b>dublicate</b>";
+                    print"<b>duplicate</b>";
                     $sql2 = mysqli_query($db->conn, "DELETE FROM paal WHERE id = '$id'");
                 }
             }
--- WebApp/pages/SelectPlace.php
+++ WebApp/pages/SelectPlace.php
@@ -34,10 +34,14 @@ else if($kind == "Park")
 <div id="map" style="width:100%;height:400px;"></div>
 
 <script>
+    var long1 = "4.4837392";
+    var lat1 = "51.9365139";
+
     var long = "<?php print $long ?>";
     var lat = "<?php print $lat ?>";
 
     var myCenter = new google.maps.LatLng(long,lat);
+    var myCenter1 = new google.maps.LatLng(long1,lat1);
     function myMap() {
         var myCenter = new google.maps.LatLng(lat,long);
         var mapCanvas = document.getElementById("map");
@@ -45,6 +49,8 @@ else if($kind == "Park")
         var map = new google.maps.Map(mapCanvas, mapOptions);
         var marker = new google.maps.Marker({position:myCenter});
         marker.setMap(map);
+        var marker1 = new google.maps.Marker({position:myCenter1});
+        marker1.setMap(map);
     }
 
 </script>
