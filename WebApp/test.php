f7a7d3d4d5a9cffb4cf0b11b64a40422fc0774f0
JoZonneveld <joostz14@gmail.com>
2017-04-11 21:21:23 +0200

    home merge

--- WebApp/test.php
+++ WebApp/test.php
@@ -1,14 +1,31 @@
 <script>
-    var objConnection = new ActiveXObject("adodb.connection");
-    var strConn = "driver={sql server};server=localhost;database=project3;uid=root;password=";
-    objConnection.Open(strConn);
-    var rs = new ActiveXObject("ADODB.Recordset");
-    var strQuery = "SELECT * FROM  paal";
-    rs.Open(strQuery, objConnection);
-    rs.MoveFirst();
-    while (!rs.EOF)
-    {
-        document.write(rs.fields("Adres"));
-        rs.movenext();
+    var locations = [ ];
+</script>
+
+
+<script type="text/javascript">
+    locations.push(['<?= $adres[1] ?>', "<?= $lat[1] ?>", "<?= $long[1] ?>", 1)
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 13,
+        center: new google.maps.LatLng(51.9244201, 4.4777325),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    for (i = 0; i < locations.length; i++) {
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+            map: map
+        });
+
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                infowindow.setContent(locations[i][0]);
+                infowindow.open(map, marker);
+            }
+        })(marker, i));
     }
 </script>
