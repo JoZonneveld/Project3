95fe5a85fc60402809c5b878d73b7df9311b0399
RoosLamens <roos-lamens@hotmail.com>
2017-04-11 15:38:04 +0200

    .

--- App/Database/src/Database/Main.java
+++ App/Database/src/Database/Main.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -17,13 +19,23 @@ public class Main {
             System.out.println("Opened database successfully");
 
             stmt = c.createStatement();
-            ResultSet rs = stmt.executeQuery( "SELECT DISTINCT Adres, `Longitude`, `Latitude` FROM `paal` ORDER BY Adres;" );
+            ResultSet rs = stmt.executeQuery( "SELECT id, Adres, Longitude, Latitude FROM paal ORDER BY Adres;" );
+
+            List<String[]> test = new ArrayList<>();
+
+
             while ( rs.next() ) {
 
                 String Adres = rs.getString("Adres");
+                String Longitude = rs.getString("Longitude");
+                String Latitude = rs.getString("Latitude");
+                // schrijf een code die Adres, Longtitude en latitude toevoegd aan de array.
+                test.add(new String[] {Adres, Longitude, Latitude});
 
-                System.out.println( "Adres = " + Adres );
+                System.out.println( Adres + " - " + Longitude + " - " + Latitude);
             }
+            System.out.println(test.get(0)[2]);
+
             rs.close();
             stmt.close();
             c.close();
